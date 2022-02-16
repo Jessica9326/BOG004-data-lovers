e2f34eef9d50c31c0d84266cc6bf0d61f79a4213
LilianaRuizC <angoraazul@hotmail.com>
2022-02-16 07:22:47 -0600

    test filtro estado

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,5 @@
 //import { example, anotherExample } from '../src/data.js';
-import {filtrarPersonajesEspecie} from '../src/data.js';
+import {filtrarPersonajesEspecie, filtrarPersonajesEstado} from '../src/data.js';
 
 
 describe('filtrosPorEspecie', () => {
@@ -15,14 +15,37 @@ describe('filtrosPorEspecie', () => {
   it('deberia Retornar un Array de 3 elementos', () => {
     expect(filtrarPersonajesEspecie("Vampire").length).toBe(3);
   });
-
-  // it('returns `example`', () => {
-  //   expect(example()).toBe('example');
-  // });
 });
 
+ describe('filtrosPorEstado', () => {
+  
+   it('deberia Retornar un Array', () => {
+      expect(Array.isArray(filtrarPersonajesEstado("Alive"))).toBe(true);
+    });
+  
+    it('deberia Retornar un Array de 65 elementos', () => {
+      expect(filtrarPersonajesEstado("unknown").length).toBe(65);
+   });
+  
+   it('deberia Retornar un Array de 147 elementos', () => {
+    expect(filtrarPersonajesEstado("Dead").length).toBe(147);
+   });
+});
 
-// describe('anotherExample', () => {
+//describe("personajes.filter(p => p.name.toLowerCase().includes(criterio.toLowerCase()))",() =>{ 
+  ///it('debería ser una función', () => {
+    //expect(typeof personajes.filter(p => p.name.toLowerCase().includes(criterio.toLowerCase())) ).toBe('function');
+ // }); 
+ //});
+
+
+           // it('returns `example`', () => {
+          //   expect(example()).toBe('example');
+          // });
+
+
+
+        // describe('anotherExample', () => {
 //   it('is a function', () => {
 //     expect(typeof anotherExample).toBe('function');
 //   });
