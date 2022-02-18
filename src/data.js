7d9e25b1187a1607ca9260d056b6f9e615f8cdcd
Jessica Sierra <jessica.sierraramos@gmail.com>
2022-02-18 15:11:01 -0500

    libreria Chart.js

--- src/data.js
+++ src/data.js
@@ -8,13 +8,12 @@
   //return 'OMG';
 //};
 import data from './data/rickandmorty/rickandmorty.js';
-
+//import {Chart} from '../node_modules/chart.js/dist/chart.min.js';
 
 export const personajes = data.results; 
 
 //  let status = personajes.filter(personaje => personaje.status=="Alive")
- 
-//  let species= personajes.filter(personaje => personaje.species=="")
+//  let species= personajes.filte(personaje => personaje.species=="")
 
 //  let name= personajes.filter(personaje => personaje.name=="")
 
@@ -55,12 +54,39 @@ export const episodios = (personaje) =>{
  return personaje.episode.length
 }
 
-
-
-
-
-
-
-
-
-
+// const graficaEspecie = document.getElementById('myChart');
+// const myChart = new Chart(graficaEspecie, {
+//     type: 'bar',
+//     data: {
+//       labels: ["Alien", "Robot", "Humano", "Humanoide", "Mitólogico", "Poppybutthole", "Animal", "Vampiro", "Parásito", "Enfermedad", "Cronenbe"],
+//       datasets: [{
+//         //label: 'Especies',
+//         data: [filtrarPersonajesEspecie("Alien").length, filtrarPersonajesEspecie("Robot").length],
+//         backgroundColor: [
+//           'rgba(255, 99, 132, 0.2)',
+//           'rgba(54, 162, 235, 0.2)',
+//           'rgba(255, 206, 86, 0.2)',                  
+//           'rgba(75, 192, 192, 0.2)',
+//           'rgba(153, 102, 255, 0.2)',
+//           'rgba(255, 159, 64, 0.2)'
+//       ],
+//       borderColor: [
+//           'rgba(255, 99, 132, 1)',
+//           'rgba(54, 162, 235, 1)',
+//           'rgba(255, 206, 86, 1)',
+//           'rgba(75, 192, 192, 1)',
+//           'rgba(153, 102, 255, 1)',
+//           'rgba(255, 159, 64, 1)'
+//       ],
+//       borderWidth: 1
+//   }]
+// },
+// options: {
+//   scales: {
+//       y: {
+//           beginAtZero: true
+//         }
+//       }
+//   }
+// });
+  
